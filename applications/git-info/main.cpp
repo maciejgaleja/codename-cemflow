7952e973725a0935f9d8f7b2538d1e9f41378f3a
Maciej Galeja <maciej.galeja@outlook.com>
2020-05-14 19:22:34 +0200

    Format code

--- applications/git-info/main.cpp
+++ applications/git-info/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+/* @file          main.cpp                                                    */
 
 #include <git2.h>
+#include <iostream>
 
-int main(int argc, char**argv)
-{ git_libgit2_init();
+int main(int argc, char** argv)
+{
+    git_libgit2_init();
     std::cout << "Hello, world\n";
     git_libgit2_shutdown();
     return 0;
