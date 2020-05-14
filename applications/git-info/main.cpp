f1850293ef529ffb7326b073e3cb74b2040294e0
Maciej Galeja <maciej.galeja@outlook.com>
2020-05-14 19:20:18 +0200

    Remove libgit2 submodule

--- applications/git-info/main.cpp
+++ applications/git-info/main.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+
+#include <git2.h>
+
 int main(int argc, char**argv)
-{
+{ git_libgit2_init();
+    std::cout << "Hello, world\n";
+    git_libgit2_shutdown();
     return 0;
 }
